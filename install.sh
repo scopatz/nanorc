3fe0e54a3686c7b4015331c279e1fa3af0754c2c
myersg86 <myersg86@gmail.com>
2017-03-06 15:16:15 -0800

    Fix broken install script

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-function _fetch_sources() {
+
   wget -O /tmp/nanorc.zip https://github.com/scopatz/nanorc/archive/master.zip
   if [ ! -d ~/.nano/ ]
   then
@@ -13,40 +13,17 @@ function _fetch_sources() {
   mv nanorc-master/* ./
   rm -rf nanorc-master
   rm /tmp/nanorc.zip
-}
 
-function _update_nanorc() {
+
   if [ ! -f ~/.nanorc ]
   then
       touch ~/.nanorc
   fi
 
   # add all includes from ~/.nano/nanorc if they're not already there
+  NANORC_FILE=~/.nanorc
   while read inc; do
       if ! grep -q "$inc" "${NANORC_FILE}"; then
           echo "$inc" >> $NANORC_FILE
       fi
   done < ~/.nano/nanorc
-}
-
-function _update_nanorc_lite() {
-  sed -i '/include "\/usr\/share\/nano\/\*\.nanorc"/i include "~\/.nano\/*.nanorc"' "${NANORC_FILE}"
-}
-
-NANORC_FILE=~/.nanorc
-
-case "$1" in
- -l|--lite)
- UPDATE_LITE=1;;
- -h|--help)
- echo "Install script for nanorc syntax highlights"
- echo "Call with -l or --lite to update .nanorc with secondary precedence to existing .nanorc includes"
- ;;
-esac
-
-_fetch_sources;
-if [[ $UPDATE_LITE ]]; then
-  _update_nanorc_lite;
-else
-  _update_nanorc;
-fi
