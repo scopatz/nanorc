9ed81c37b0a77e83201f9f1bbe1d4d81efcca6a9
Tiago Almeida <tjamadeira@gmail.com>
2019-11-05 00:42:09 +0000

    Add help font

--- install.sh
+++ install.sh
@@ -15,6 +15,10 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <https://www.gnu.org/licenses/>.
 
+# Help:
+# https://www.shellscript.sh/tips/getopts/
+
+
 # Bash Variables
 # Ativate or not the erros (1=activated)
 OPTERR=1
